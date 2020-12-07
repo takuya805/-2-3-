5e8a03e1bc74b83075d949cbe5168034df67fe73
takuya805 <takuya0903exile@gmail.com>
2020-12-07 02:36:33 +0000

    development2-再提出

--- bookers2/app/controllers/users_controller.rb
+++ bookers2/app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
   before_action :authenticate_user!
+
   def index
     @users = User.all
     @user = User.find(current_user.id)
@@ -42,6 +43,6 @@ class UsersController < ApplicationController
 
   private
   def user_params
-    params.require(:user).permit(:name, :introduction, :profile_image_id)
+    params.require(:user).permit(:name, :introduction, :profile_image)
   end
 end
--- bookers2/config/initializers/application_controller_renderer.rb
+++ bookers2/config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = 'bc633ce6252bfe8d1bb28dcc998d6dbd4fd543cf139be4803f58f7a04b214c3169555319e0ae74c612d42319f89d43ff854aef1418d662bbebcc1fbfa9273b6f'
